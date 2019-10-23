869e57d3ccc649ad2f52300032a8f580f307ab90
sddtc <changhbaga@gmail.com>
2019-10-24 00:35:48 +0800

    Step 10

--- src/sharing/session-3.ts
+++ src/sharing/session-3.ts
@@ -11,3 +11,15 @@ export const yell = (str: any) => {
 
   return str.toUpperCase();
 };
+
+function assertIsDefined<T>(val: T): asserts val is NonNullable<T> {
+  if (val === undefined || val === null) {
+    throw new AssertionError({ message: `Expected 'val' to be defined, but received ${val}`});
+  };
+};
+
+export const yellPlus = (str: any) => {
+  assertIsDefined(str);
+
+  return str.toUpperCase();
+};
--- test/sharing/session-3.test.ts
+++ test/sharing/session-3.test.ts
@@ -1,4 +1,4 @@
-import { yell } from '../../src/sharing/session-3';
+import { yell, yellPlus } from '../../src/sharing/session-3';
 
 describe('Assertion Functions', () => {
   it('asserts condition with not string', () => {
@@ -12,5 +12,27 @@ describe('Assertion Functions', () => {
 
   it('asserts condition with string', () => {
     expect(yell('hello')).toEqual('HELLO');
+  });
+
+  it('str is null', () => {
+    try {
+      yellPlus(null);
+      fail();
+    } catch (e) {
+      expect(e.message).toEqual('Expected \'val\' to be defined, but received null');
+    }
+  });
+
+  it('str is undefined', () => {
+    try {
+      yellPlus(undefined);
+      fail();
+    } catch (e) {
+      expect(e.message).toEqual('Expected \'val\' to be defined, but received undefined');
+    }
+  });
+
+  it('asserts condition with string', () => {
+    expect(yellPlus('hello')).toEqual('HELLO');
   });
 });
